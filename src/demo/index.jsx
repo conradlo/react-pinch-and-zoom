46b3d5d0a3edf05fa85a5d2959f79a8c1c10b64b
Conrad Lo <email@conradlo.com>
2018-11-07 00:50:30 +0800

    [npm] run lint and test task on prebuild

--- src/demo/index.jsx
+++ src/demo/index.jsx
@@ -4,5 +4,5 @@ import { render } from 'react-dom'
 import App from './App'
 import './css/w3.css'
 
-console.log('[src/demo/script.js] ', document.body.clientWidth)
+console.log('[src/demo/script.js] ', document.body.clientWidth) // eslint-disable-line no-console
 render(<App />, document.getElementById('app'))
